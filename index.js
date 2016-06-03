e951cd9d5d4ae74e16321636babf29d1bc5addab
joelroxell <joel.roxell@na-kd.com>
2016-06-03 16:51:10 +0200

    Changed export src

--- index.js
+++ index.js
@@ -1,1 +1,3 @@
-module.exports = require('./lib')
+import Clio from 'src';
+
+export default Clio;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,3 @@
 import Clio from './clio';
-import log from './models/log';
 
-const loader = (config = {}) => new Clio(config.host, config.env);
-
-export {
-  Clio,
-  loader
-};
+export { Clio };
