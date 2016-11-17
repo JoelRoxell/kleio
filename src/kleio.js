349adc72af6cc6a62d1a04248378943c4c443046
Joel Roxell <joel.roxell@na-kd.com>
2016-11-17 10:12:46 +0100

    kleio: reorder parameters in order to improve functional resuse.

--- src/kleio.js
+++ src/kleio.js
@@ -76,12 +76,12 @@ function consoleLog(message, severity, meta) {
  * Logger.
  * @param  {String} env      Environment variable, (development|production)
  * @param  {Function} post   Callback used to post log to remote server, only called in prodcution.
- * @param  {String} message  Log description.
  * @param  {Number} severity Integer describing the error level.
+ * @param  {String} message  Log description.
  * @param  {mixed}  meta     Optional meta data that might be valuable during debugging.
  * @return {Promise}         Use this if additional tasks or error managments must be performed after a log entry.
  */
-function core(env, post, message, severity, meta) {
+function core(env, post, severity, message, meta) {
   if (env === 'production') {
     return new Promise(function(resolve, reject) {
       try {
--- test/kleio-test.js
+++ test/kleio-test.js
@@ -23,20 +23,22 @@ describe('kleio', function() {
 
   it('should currey and call the function if all parameters are passed.', function() {
     const callback = sinon.spy();
-    const log = kleio('production')(callback)('msg')(LEVELS.SILENT)({});
+
+    kleio('production')(callback)(LEVELS.SILENT)('msg')({});
 
     expect(callback.calledOnce).to.equal(true);
   });
 
   it('should not run post callback in development mode', function() {
     const callback = sinon.spy();
-    const log = kleio('development')(callback)('msg')(LEVELS.SILENT)({});
+
+    kleio('development')(callback)(LEVELS.SILENT)('msg')({});
 
     expect(callback.calledOnce).to.equal(false);
   });
 
   it('should use use post function and throw error', function(done) {
-    log(R._)(LEVELS.ERROR)({}).catch(err => {
+    log(LEVELS.ERROR)(R._)({}).catch(err => {
       expect(err).to.exist;
 
       done();
@@ -44,7 +46,7 @@ describe('kleio', function() {
   });
 
   it('should use use post function and succeed', function(done) {
-    log('msg')(LEVELS.ERROR)({}).then(res => {
+    log(LEVELS.ERROR)('msg')({}).then(res => {
       expect(res).to.have.property('message', 'msg');
       expect(res).to.have.property('severity', 0);
 
